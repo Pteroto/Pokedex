7884f61274498c055f0f10232b218645a806f00d
Gustavo Monteiro <gustavosmonteiro@outlook.com>
2021-04-13 22:52:48 -0300

    Estudo sobre threads, enqueue de forma implicita e execute de forma explicita

--- app/src/main/java/com/doubleg/pokedex/MainActivity.kt
+++ app/src/main/java/com/doubleg/pokedex/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.doubleg.pokedex
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Callback
@@ -9,32 +12,60 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val retrofit = Retrofit.Builder()
-                .baseUrl("https://pokeapi.co/api/v2/")
-                .addConverterFactory(MoshiConverterFactory.create())
-                .build()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
 
         val service: PokemonService = retrofit.create(PokemonService::class.java)
 
-        service.getPokemon("bulbasaur").enqueue(object : Callback<Pokemon> {
-            override fun onResponse(call: Call<Pokemon>, response: Response<Pokemon>) {
-                if (response.isSuccessful) {
-                    val name = response.body()?.name
-                    Log.d("TESTE", name ?: "")
-                    val id = response.body()?.id
-                    Log.d("TESTE", id.toString())
-                }
-            }
+        val button = findViewById<Button>(R.id.button)
 
-            override fun onFailure(call: Call<Pokemon>, t: Throwable) {
-                Log.d("TESTE", t.message ?: "")
-            }
-        })
+        //enqueueExample(button, service)
+        executeExample(button, service)
+    }
+
+    private fun enqueueExample(button: Button, service: PokemonService) {
+        button.setOnClickListener {
+            service.getPokemon("bulbasaur").enqueue(object : Callback<Pokemon> {
+                override fun onResponse(call: Call<Pokemon>, response: Response<Pokemon>) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(this@MainActivity, response.body()?.name, Toast.LENGTH_SHORT)
+                            .show()
+                        val name = response.body()?.name
+                        Log.d("TESTE", name ?: "")
+                        val id = response.body()?.id
+                        Log.d("TESTE", id.toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<Pokemon>, t: Throwable) {
+                    Log.d("TESTE", t.message ?: "")
+                }
+            })
+        }
+    }
+
+    private fun executeExample(button: Button, service: PokemonService) {
+        button.setOnClickListener {
+            //MainThread
+            AsyncTask.execute(object : Runnable {
+                override fun run() {
+                    //BackgroundThread
+                    val response = service.getPokemon("pikachu").execute()
+                    runOnUiThread(object : Runnable {
+                        override fun run() {
+                            //MainThread
+                            Toast.makeText(this@MainActivity, response.body()?.name, Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                }
+            })
+        }
     }
 }
